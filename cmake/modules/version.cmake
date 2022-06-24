f0573f45c48b87caf69e03a67723f74f264833c2
nodedev74 <87693559+nodedev74@users.noreply.github.com>
2022-06-24 18:41:29 +0200

    bin include

---
+++ cmake/modules/version.cmake
@@ -0,0 +1,13 @@
+############################
+### Author: nodedev74
+### Created: 23-06-22
+### Purpose: version handler
+#############################
+
+# updates cn/version.h
+
+function(update_version input output)
+    
+
+endfunction(update_version input output)
+
---
+++ include/cn/version.h
@@ -0,0 +1,19 @@
+// -------------------------------
+// -- This file is auto generated 
+// --  !! Dont change content !!
+// -------------------------------
+
+#ifndef VERSION_H
+#define VERSION_H
+
+// https://www.akeeba.com/how-do-version-numbers-work.html
+
+#define VERSION_ENABLE
+
+#define MAJOR_VERSION 1
+/* #undef MINOR_VERSION */
+/* #undef RELEASE_NUMBER */
+
+/* #undef CN_VERSION */
+
+#endif
