031c8fa1c4ed2cadd4669af09555d30c5c32c533
nodedev74 <87693559+nodedev74@users.noreply.github.com>
2022-06-25 14:22:23 +0200

    compile process done

--- build/bootstrap.sh
+++ build/bootstrap.sh
@@ -4,4 +4,6 @@ cd "./temp" || exit
 
 cmake -S ../../ -Brelease -DCMAKE_BUILD_TYPE=Release
 
+printf "\n[LOG] | Prepared successfully"
+
 read -r
--- build/build.sh
+++ build/build.sh
@@ -1,5 +1,11 @@
 #! /bin/sh
 
-cmake --build ./temp/release --config Release
+DIR=./temp/release
+
+if [ -d "$DIR" ]; then
+    cmake --build ./temp/release --config Release
+else
+    echo "[ERROR] | run build/bootstrap.sh to prepare build"
+fi
 
 read -r
