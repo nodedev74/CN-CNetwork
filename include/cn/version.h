2f3f624ec4b215137ddfec0a597ba49599493c62
nodedev74 <87693559+nodedev74@users.noreply.github.com>
2022-07-05 21:40:11 +0200

    src code comments

--- include/cn/version.h
+++ include/cn/version.h
@@ -1,16 +1,19 @@
-// -------------------------------
-// -- This file is auto generated
-// --  !! Dont change content !!
-// -------------------------------
+/////////////////////////////////////////////////////////////////////////////
+// Name:        cn/version.h
+// Purpose:     CN version infos
+// Author:      nodedev74
+// Created:     2022-07-05
+// Copyright:   (c) 2022 Lenard Büsing
+/////////////////////////////////////////////////////////////////////////////
 
 #ifndef VERSION_H
 #define VERSION_H
 
 // https://www.akeeba.com/how-do-version-numbers-work.html
 
-#define MAJOR_VERSION 1
-#define MINOR_VERSION 0
-#define RELEASE_NUMBER 0
+#define MAJOR_VERSION @MAJOR_VERSION @
+#define MINOR_VERSION @MINOR_VERSION @
+#define RELEASE_NUMBER @RELEASE_NUMBER @
 
 #define CN_VERSION "1.0.0"
 
